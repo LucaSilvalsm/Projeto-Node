200e4111fdfbef0ff90fa01f4916b9773ae66d95
Lucas Moreira.pub <Lucas.silvalsm@hotmail.com>
2026-02-03 14:34:22 -0300

    Adicionando Articles

--- Articles/ArticlesController.js
+++ Articles/ArticlesController.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Articles = require('./Article'); // seu model
 const slugify = require('slugify');    // cria url amigável
 const flash = require('connect-flash'); // middleware do flash
+const Category = require('../categories/Category'); // seu model de categorias
 
 // rota de teste
 router.get("/articles", (req, res) => {
@@ -11,7 +12,13 @@ router.get("/articles", (req, res) => {
 
 // rota para renderizar o formulário de novo artigo
 router.get("/admin/articles/new", (req, res) => {
-    res.render("admin/articles/new"); // views/admin/articles/new.ejs
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    }).catch(err => {
+        req.flash("error", "Erro ao carregar categorias");
+        res.redirect("/admin/articles");
+    });
+   
 });
 
 // exportando corretamente
